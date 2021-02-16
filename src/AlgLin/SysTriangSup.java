94afe6e4a21b1cb93d45f051af2702de8a95a0e3
Justine Lemesle <justine.lemesle2000@gmail.com>
2021-02-16 15:09:20 +0100

    sys inf fonctionne

--- src/AlgLin/SysTriangSup.java
+++ src/AlgLin/SysTriangSup.java
@@ -14,13 +14,25 @@ public class SysTriangSup extends SysLin{
 		composants[0] = 4;
 		composants[1] = 5;
 		Vecteur vecteur = new Vecteur(composants);
-		double mat[][]= {{2,1},{3,2}};
+		double mat[][]= {{2,1},{0,2}};
 		Matrice matrice = new Matrice(mat);								//2x + 1y = 4
-		SysLin sys = new SysTriangSup(matrice, vecteur);				//3x + 2y = 5
-		sys.resolution();
+		SysLin sys = new SysTriangSup(matrice, vecteur);				//0x + 2y = 5
+		System.out.println(sys.resolution());
 	}
 
 	public Vecteur resolution() throws IrregularSysLinException {
-		return null;
+		Vecteur resultat = new Vecteur(getMatriceSystem().nbLigne());
+
+		double sum, coeff;
+
+		for(int i=0;i<getMatriceSystem().nbLigne();i++) {
+			sum=0;
+			for(int j=0; j<i;j++) {
+				sum=sum+getMatriceSystem().getCoef(i,j)*resultat.getCoef(j);
+			}
+			coeff=(getsecondMembre().getCoef(i)-sum)/getMatriceSystem().getCoef(i, i);
+			resultat.remplacecoef(i, coeff);
+		}
+		return resultat;
 	}
 }
