057abc89198aa77a323d94341144af78bd1ac411
Julien Deveaux <julien.deveaux@hotmail.com>
2021-02-16 15:56:00 +0100

    Fix Unite

--- src/AlgLin/SysTriangInfUnite.java
+++ src/AlgLin/SysTriangInfUnite.java
@@ -1,6 +1,22 @@
 package AlgLin;
 
 public class SysTriangInfUnite extends SysTriangInf {
+	public static SysTriangInfUnite make(Matrice m, Vecteur v) throws Exception {
+		int  j = 0;
+		boolean b = true;
+		for(int i = 0; i < m.nbLigne(); i++) {
+			if(m.getCoef(i, j) != 1) {
+				b = false;
+			}
+			j++;
+		}
+		if(b) {
+			SysTriangInfUnite sys = new SysTriangInfUnite(m, v);
+			return sys;
+		}
+		return null;
+	}
+
 	public SysTriangInfUnite(Matrice m, Vecteur v) throws Exception{
 		super(m, v);
 	}
@@ -12,7 +28,7 @@ public class SysTriangInfUnite extends SysTriangInf {
 		Vecteur vecteur = new Vecteur(composants);
 		double mat[][]= {{1, 0},{3, 1}};				//1x + 0y = 4
 		Matrice matrice = new Matrice(mat);				//3x + 1y = 5
-		SysLin sys = new SysTriangInfUnite(matrice, vecteur);
+		SysLin sys = make(matrice, vecteur);
 		System.out.println(sys.resolution());
 	}
 }
--- src/AlgLin/SysTriangSupUnite.java
+++ src/AlgLin/SysTriangSupUnite.java
@@ -1,7 +1,23 @@
 package AlgLin;
 
 public class SysTriangSupUnite extends SysTriangSup {
-	public SysTriangSupUnite(Matrice m, Vecteur v) throws Exception {
+	public static SysTriangSupUnite make(Matrice m, Vecteur v) throws Exception {
+		int  j = 0;
+		boolean b = true;
+		for(int i = 0; i < m.nbLigne(); i++) {
+			if(m.getCoef(i, j) != 1) {
+				b = false;
+			}
+			j++;
+		}
+		if(b) {
+			SysTriangSupUnite sys = new SysTriangSupUnite(m, v);
+			return sys;
+		}
+		return null;
+	}
+
+	private SysTriangSupUnite(Matrice m, Vecteur v) throws Exception {
 		super(m, v);
 	}
 
@@ -12,7 +28,7 @@ public class SysTriangSupUnite extends SysTriangSup {
 		Vecteur vecteur = new Vecteur(composants);
 		double mat[][]= {{1, 3},{0, 1}};				//1x + 3y = 4
 		Matrice matrice = new Matrice(mat);				//0x + 1y = 5
-		SysLin sys = new SysTriangSupUnite(matrice, vecteur);
+		SysLin sys = make(matrice, vecteur);
 		System.out.println(sys.resolution());
 	}
 }
