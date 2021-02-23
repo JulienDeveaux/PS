5691171ca882421cde9d5d365a7084af4a912797
Julien Deveaux <julien.deveaux@hotmail.com>
2021-02-23 13:45:49 +0100

    Ajout resources + refactor style

--- src/AlgLin/Helder.java
+++ src/AlgLin/Helder.java
@@ -1,52 +1,50 @@
 package AlgLin;
 
-public class Helder extends SysLin
-{
+public class Helder extends SysLin {
 
     public Helder(Matrice m, Vecteur v) throws  IrregularSysLinException  {
         super(m, v);
     }
 
     public void factorLDR() throws IrregularSysLinException  {
+        SysTriangInfUnite L = new SysTriangInfUnite(matriceSystem, secondMembre);
+        SysDiagonal D = new SysDiagonal(matriceSystem, secondMembre);
+        SysTriangSupUnite R = new SysTriangSupUnite(matriceSystem, secondMembre);
 
-        SysTriangInfUnite l = new SysTriangInfUnite( matriceSystem, secondMembre );
-        SysDiagonal d = new SysDiagonal( matriceSystem, secondMembre );
-        SysTriangSupUnite r = new SysTriangSupUnite( matriceSystem, secondMembre );
-
-        Matrice.produit(matriceSystem, l.matriceSystem);
-        Matrice.produit(matriceSystem, d.matriceSystem);
-        Matrice.produit(matriceSystem, r.matriceSystem);
+        Matrice.produit(matriceSystem, L.matriceSystem);
+        Matrice.produit(matriceSystem, D.matriceSystem);
+        Matrice.produit(matriceSystem, R.matriceSystem);
     }
 
     @Override
     public Vecteur resolution() throws IrregularSysLinException {
         factorLDR();
-        SysTriangInfUnite l = new SysTriangInfUnite(matriceSystem, secondMembre);
-        SysDiagonal  d = new SysDiagonal(matriceSystem, l.resolution());
-        SysTriangSupUnite r = new SysTriangSupUnite(matriceSystem, d.resolution());
+        SysTriangInfUnite L = new SysTriangInfUnite(matriceSystem, secondMembre);
+        SysDiagonal D = new SysDiagonal(matriceSystem, L.resolution());
+        SysTriangSupUnite R = new SysTriangSupUnite(matriceSystem, D.resolution());
 
-        return r.resolution();
+        return R.resolution();
     }
 
     public Vecteur resolutionPartielle() throws IrregularSysLinException {
-        SysTriangInfUnite l = new SysTriangInfUnite(matriceSystem, secondMembre);
-        SysDiagonal d = new SysDiagonal(matriceSystem, l.resolution());
-        SysTriangSupUnite r = new SysTriangSupUnite(matriceSystem, d.resolution());
+        SysTriangInfUnite L = new SysTriangInfUnite(matriceSystem, secondMembre);
+        SysDiagonal D = new SysDiagonal(matriceSystem, L.resolution());
+        SysTriangSupUnite R = new SysTriangSupUnite(matriceSystem, D.resolution());
 
-        return r.resolution();
+        return R.resolution();
     }
 
-    public void setSecondMembre(Vecteur newSecondMembre) {
-        secondMembre = newSecondMembre;
+    public void setSecondMembre(Vecteur SecondMembre) {
+        this.secondMembre = SecondMembre;
     }
 
     public static void main(String[] args) {
-        Matrice m1 = new Matrice("resources/matrice1.txt");
-        Matrice m2 = new Matrice("resources/matrice1.txt");
-        Vecteur v1 = new Vecteur("resources/vecteur1.txt");
+        Matrice mat1 = new Matrice("src/resources/mat1.txt");
+        Matrice mat2 = new Matrice("src/resources/mat1.txt");
+        Vecteur vec1 = new Vecteur("src/resources/vec1.txt");
 
-        System.out.println(m1);
-        System.out.println(m2);
-        System.out.println(v1);
+        System.out.println("Matrice 1 : \n" + mat1);
+        System.out.println("Matrice 2 : \n" + mat2);
+        System.out.println("Vecteur : \n" + vec1);
     }
 }
